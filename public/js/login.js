bdb01911d72ef2267950d3bd865b00d35e48a99c
Arnaudvitale <arnaud.vitale@epitech.eu>
2023-06-17 13:09:09 +0200

    [+] modified login screen

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,55 @@
 document.getElementById("login-form").addEventListener("submit", function(event){
-    event.preventDefault(); // to stop the form from submitting
+    event.preventDefault();
     logIn();
 });
 
 document.getElementById("register-form").addEventListener("submit", function(event){
-    event.preventDefault(); // to stop the form from submitting
+    event.preventDefault();
     register();
 });
 
-function logIn() {
+document.getElementById("direct-chess").addEventListener("click", function(){
+    window.location.href = '/chess.html';
+});
+
+function logIn(event) {
+    event.preventDefault();
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (username === "admin" && password === "admin") {  // for example
-        window.location.href = '/chess';  // Redirect to the chess page
-    } else {
-        alert("Invalid username or password");
-    }
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    }).then(response => response.json()).then(data => {
+        if (data.error) {
+            alert(data.error);
+        } else {
+            window.location.href = '/chess';  // Redirect to the chess page
+        }
+    });
 }
 
-function register() {
+function register(event) {
+    event.preventDefault();
+
     const username = document.getElementById('reg-username').value;
     const password = document.getElementById('reg-password').value;
 
-    // Registration logic here
-    alert(`Registered ${username}`);
+    fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    }).then(response => response.json()).then(data => {
+        if (data.error) {
+            alert(data.error);
+        } else {
+            alert(`Registered ${username}`);
+        }
+    });
 }
