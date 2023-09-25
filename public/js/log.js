7a16ac52ce96c64edde1b4cb9b626f5c93d62d5c
Arnaudvitale <arnaud.vitale@epitech.eu>
2023-09-25 13:43:46 +0200

    [+] features for login mobile and mobile detection

--- public/js/log.js
+++ public/js/log.js
@@ -1,5 +1,30 @@
-const mediaQuery = window.matchMedia("(max-width: 600px)");
-if (mediaQuery.matches) {
+// detect user device
+function isMobile() {
+  var userAgentInfo = navigator.userAgent;
+  var mobileAgents = [
+    "Android",
+    "iPhone",
+    "SymbianOS",
+    "Windows Phone",
+    "iPad",
+    "iPod"
+  ];
+  var mobile_flag = false;
+  for (var v = 0; v < mobileAgents.length; v++) {
+    if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+      mobile_flag = true;
+      break;
+    }
+  }
+  var screen_width = window.screen.width;
+  var screen_height = window.screen.height;
+  if (screen_width < 500 && screen_height < 800) {
+    mobile_flag = true;
+  }
+  return mobile_flag;
+}
+
+if (isMobile()) {
   // phone user
   window.location.href = "/phone/loginphone.html";
 } else {
